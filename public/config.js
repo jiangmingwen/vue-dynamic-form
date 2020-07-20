9bdeaa9fd5d1bce34caa1775f804999a1f02e2c2
Jaymingwen <649731870@qq.com>
2020-07-20 09:22:32 +0800

    error commit

--- public/config.js
+++
@@ -1,6 +0,0 @@
-window.config = {
-    gateway: 'http://192.168.16.188:729',//获取网关的地址
-    gatewayConfigKey: 'SYS_CONFIG',//后端网关KEY
-    APP_CODE: 'a2d83d107b114f739fa791c2bbba13db',
-    logo_title: '开州招生报名信息平台',
-}
